df8d1e86634d1dd1f1a514a1c07477eb6a0b1ee2
zz871997 <zz15121998@gmail.com>
2017-08-03 11:52:09 +0700

    Add class DepthFirstSearchEngine
    2D Maze Search: Performs a depth first search in a maze

--- Finding_path_in_Mazes_TQK/src/Search_Engine/DepthFirstSearchEngine.java
+++ Finding_path_in_Mazes_TQK/src/Search_Engine/DepthFirstSearchEngine.java
@@ -1,5 +1,44 @@
 package Search_Engine;
 
-public class DepthFirstSearchEngine {
+/**
+ * 
+ * @author Tran Quang Khai
+ * 2D Maze Search: Performs a depth first search in a maze
+ *
+ */
 
+public class DepthFirstSearchEngine extends AbstractSearchEngine {
+
+	public DepthFirstSearchEngine(int width, int height) {
+		super(width, height);
+		iterateSearch(startLoc, 1);
+	}
+
+	private void iterateSearch(Location loc, int depth) {
+		// TODO Auto-generated method stub
+		if(isSearching == false) return;
+		maze.setValue(loc.x, loc.y, (short)depth);
+		
+		Location[] moves = getPossibleMoves(loc);
+		for(int i = 0; i < 4; i++){
+			if(moves[i] == null) break;	//out of possible from this location
+			searchPath[depth] = moves[i];
+			//If goalLoc is found
+			if(equals(moves[i], goalLoc)){
+				System.out.println("Found the goal at " + moves[i].x + ", " + moves[i].y);
+				isSearching = false;
+				maxDepth = depth;
+				return;
+			}
+			//Else
+			else{
+				iterateSearch(moves[i], depth + 1);
+				if(isSearching == false) return;
+			}
+		}
+		return;
+	}
+	
+	
+	
 }
